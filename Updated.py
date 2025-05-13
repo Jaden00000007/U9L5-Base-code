c7814208ec83b0bf716d425813d1931e800e82b9
Jaden00000007 <146095161+Jaden00000007@users.noreply.github.com>
2025-05-13 13:44:45 -0400

    Update Updated.py

--- Updated.py
+++ Updated.py
@@ -1,34 +1,25 @@
 import mysql.connector
 
+def data_base_connector():
+    connection = mysql.connector.connect(user='evanl127',
+                                      password='234642536',
+                                      host='10.8.37.226',
+                                      database='evanl127_db')
 
-connection = mysql.connector.connect(user='evanl127',
-                                  password='234642536',
-                                  host='10.8.37.226',
-                                  database='evanl127_db')
+    return connection
 
+def executeStatement(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    for row in cursor:
+        print("Period: "+ str(row[0]))
+        print("Course: " + row[1])
+        print("Room: " + row[2])
+        print("Teacher: " + row[3])
+    cursor.close()
+    connection.close()
 
-
-
-cursor = connection.cursor()
-
-
+connection = data_base_connector()
 stid = int(input("pick a student id from 1-5,000:  "))
-
-
 query = "call sched_get(" + str(stid) + ");"
-
-
-cursor.execute(query)
-
-
-for row in cursor:
-   print("Period: "+ str(row[0]))
-   print("Course: " + row[1])
-   print("Room: " + row[2])
-   print("Teacher: " + row[3])
-
-
-
-
-cursor.close()
-connection.close()
+executeStatement(connection, query)
